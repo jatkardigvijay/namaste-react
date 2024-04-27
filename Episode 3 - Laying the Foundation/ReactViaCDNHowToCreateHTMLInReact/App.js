447407663e0e84a3749982896dddde6f2dbef285
Digvijay Jatkar <jatkardigvijay@gmail.com>
2024-04-28 00:38:02 +0100

    functional component addded

--- Episode 3 - Laying the Foundation/ReactViaCDNHowToCreateHTMLInReact/App.js
+++ Episode 3 - Laying the Foundation/ReactViaCDNHowToCreateHTMLInReact/App.js
@@ -1,3 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Title = () => {
+    return (
+        <h1>
+            Namaste React !
+        </h1>
+)};
+
+// normal javascript code calling in react
+const number = 10000;
+
+const HeadingCompoment = () => {
+    return (
+        <div id="container">
+            <Title/>
+            {number}
+            <h1>Namaste dj !</h1>
+        </div>
+)};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingCompoment/>);
