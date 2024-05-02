5aa43221584c8b174e0bb2d84787144906626366
Digvijay Jatkar <jatkardigvijay@gmail.com>
2024-05-02 19:14:55 +0100

    show me the code talk is cheap create food app

---
+++ Episode 4 - Talk is cheap show me the code/ReactViaCDNHowToCreateHTMLInReact/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"></img>
+            </div>
+        <div className="">
+
+        </div>
+        </div>
+    )
+};
+
+const AppLayout = () => {
+    return (
+        <div class="app">
+            
+        </div>
+    )
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingCompoment/>);
