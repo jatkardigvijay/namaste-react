13fa08ea1f76f80d5f6660709b9b2a68a5d2ee33
Digvijay Jatkar <jatkardigvijay@gmail.com>
2024-05-13 22:57:41 +0100

    created the nav and the list of static restaurants

--- Episode 4 - Talk is cheap show me the code/ReactViaCDNHowToCreateHTMLInReact/App.js
+++ Episode 4 - Talk is cheap show me the code/ReactViaCDNHowToCreateHTMLInReact/App.js
@@ -19,10 +19,43 @@ const Header = () => {
     );
 }
 
+// Css using JavaScript Object
+// write here code and then call in {} as below
+const styleCard = {
+    backgroundColor : "#f0f0f0"
+};
+
+const RestaurantCard = () => {
+    return (
+        <div className="res-card" style={{backgroundColor : "#f0f0f0" }}>
+        <img className="res-logo"
+        alt="res-logo" src="https://b.zmtcdn.com/data/pictures/1/50691/92d9b4053ef0965120828b4fa4eecc3b.jpg?output-format=webp&fit=around|771.75:416.25&crop=771.75:416.25;*,*"/>
+            <h3>Meghana Foods</h3>
+            <h4>Biryani, North Indian, Asian</h4>
+            <h4>4.4 stars</h4>
+            <h4>38 minutes</h4>
+        </div>
+    );
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                Search
+            </div>
+            <div className="res-container">
+                <RestaurantCard/><RestaurantCard/><RestaurantCard/>
+            </div>
+        </div>
+    );
+}
+
 const AppLayout = () => {
     return (
         <div className="app">
             <Header/>
+            <Body/>
         </div>
     );
 }
